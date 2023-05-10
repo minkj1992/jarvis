cf01f6f2df84190a54cdaedd9a6b9f608d143dda
minwook <minkj1992@gmail.com>
2023-05-10 15:32:37 +0900

    fix: ChatOpenAI read_timeout_error
    **error below**
    ```
    websockets.exceptions.ConnectionClosedOK: received 1005 (no status code [internal]); then sent 1005 (no status code [internal])
    INFO:     connection closed
    WARNING:langchain.embeddings.openai:Retrying langchain.embeddings.openai.embed_with_retry.<locals>._embed_with_retry in 4.0 seconds as it raised Timeout: Request timed out: HTTPSConnectionPool(host='api.openai.com', port=443): Read timed out. (read timeout=600).
    ERROR:root:received 1005 (no status code [internal]); then sent 1005 (no status code [internal])
    ```

--- src/infra/ai.py
+++ src/infra/ai.py
@@ -18,7 +18,7 @@ from infra.config import get_config
 from infra.jbs4 import extract_doc_metadata_from_url
 
 _cfg = get_config()
-
+_CHAT_OPEN_AI_TIMEOUT=120
 
 DEFAULT_PROMPT_TEMPLATE = """Use the following pieces of context to answer the question at the end in korean. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
@@ -27,33 +27,6 @@ DEFAULT_PROMPT_TEMPLATE = """Use the following pieces of context to answer the q
 Question: {question}
 !IMPORTANT Answer in korean:"""
 
-
-class MyChain(ConversationalRetrievalChain):
-    async def _aget_docs(self, question: str, inputs: Any):
-        try:
-            docs = await self.retriever.aget_relevant_documents(question)
-        except Exception as err:
-            logging.error(f'Question: {question}, Inputs: {inputs}')
-            logging.error(err)
-            raise err
-        result = await self._reduce_tokens_below_limit(docs)
-        return result
-    
-    async def _reduce_tokens_below_limit(self, docs: List[Any]) -> List[Any]:
-        num_docs = len(docs)
-
-        if self.max_tokens_limit:
-            tokens = [
-                self.combine_docs_chain.llm_chain.llm.get_num_tokens(doc.page_content)
-                for doc in docs
-            ]
-
-            token_count = sum(tokens[:num_docs])
-            while token_count > self.max_tokens_limit:
-                num_docs -= 1
-                token_count -= tokens[num_docs]
-
-        return docs[:num_docs]
 
 async def get_docs_from_texts(texts:str):
     docs = []
@@ -82,10 +55,44 @@ async def get_docs_and_metadatas_from_urls(urls):
 async def get_chain(vs: VectorStore, prompt:str)-> ConversationalRetrievalChain:
     qa_prompt = PromptTemplate(template=prompt, input_variables=["context", "question"])
     return ConversationalRetrievalChain.from_llm(
-        ChatOpenAI(openai_api_key=_cfg.openai_api_key, temperature=0.7, model_name="gpt-3.5-turbo"),
+        ChatOpenAI(
+            openai_api_key=_cfg.openai_api_key, 
+            temperature=0.7, 
+            model_name="gpt-3.5-turbo",
+            request_timeout=_CHAT_OPEN_AI_TIMEOUT,
+        ),
         retriever=vs.as_retriever(search_kwargs={'k':4}),
         qa_prompt=qa_prompt,
     )
+
+
+class MyChain(ConversationalRetrievalChain):
+    async def _aget_docs(self, question: str, inputs: Any):
+        try:
+            docs = await self.retriever.aget_relevant_documents(question)
+            logging.error(f'Question: {question}, Inputs: {inputs}')
+        except Exception as err:
+            logging.error(err)
+            raise err
+        result = await self._reduce_tokens_below_limit(docs)
+        return result
+    
+    async def _reduce_tokens_below_limit(self, docs: List[Any]) -> List[Any]:
+        num_docs = len(docs)
+
+        if self.max_tokens_limit:
+            tokens = [
+                self.combine_docs_chain.llm_chain.llm.get_num_tokens(doc.page_content)
+                for doc in docs
+            ]
+
+            token_count = sum(tokens[:num_docs])
+            while token_count > self.max_tokens_limit:
+                num_docs -= 1
+                token_count -= tokens[num_docs]
+
+        return docs[:num_docs]
+
 
 
 async def get_chain_stream(vs: VectorStore, prompt:str, question_handler:AsyncCallbackHandler , stream_handler: AsyncCallbackHandler):
@@ -95,8 +102,10 @@ async def get_chain_stream(vs: VectorStore, prompt:str, question_handler:AsyncCa
     question_generator = LLMChain(
         llm=ChatOpenAI(
             openai_api_key=_cfg.openai_api_key,
-            temperature=1,
-            callback_manager=AsyncCallbackManager([question_handler])
+            temperature=0.7,
+            callback_manager=AsyncCallbackManager([question_handler]),
+            request_timeout=_CHAT_OPEN_AI_TIMEOUT,
+            model_name="gpt-3.5-turbo",
             ),
         prompt=CONDENSE_QUESTION_PROMPT,
         callback_manager=manager,
@@ -104,9 +113,11 @@ async def get_chain_stream(vs: VectorStore, prompt:str, question_handler:AsyncCa
     )
     streaming_llm = ChatOpenAI(
         streaming=True,
-        temperature=1,
+        temperature=0.7,
         openai_api_key=_cfg.openai_api_key, 
         callback_manager=AsyncCallbackManager([stream_handler]), 
+        request_timeout=_CHAT_OPEN_AI_TIMEOUT,
+        model_name="gpt-3.5-turbo",
         verbose=True,
     )
     
