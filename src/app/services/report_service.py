d2b275a45284c73a4ee405f5b8c29b04751d27e1
minkj1992 <minkj1992@gmail.com>
2023-05-31 03:06:21 +0900

    fix:mistypo

--- src/app/services/report_service.py
+++ src/app/services/report_service.py
@@ -35,7 +35,7 @@ async def generate_a_report(room_uuid:str, query: str):
     reports = await llm.get_a_relationship_report_from_llm(vectorstore, topic=query, summary=summary)
     for i, report in enumerate(reports):
         result += f'--------------REPORT{i+1}--------------\n'
-        reulst += report
+        result += report
         result += '-----------------------------------\n'
 
     return result
